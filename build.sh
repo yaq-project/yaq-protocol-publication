968edccdd8a17ea592c7a7fcf28937daa4b0f27c
Blaise Thompson <blaise@untzag.com>
2023-03-16 13:00:17 -0500

    diff

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+
+# manuscript
 pdflatex --interaction=nonstopmode paper.tex
 bibtex paper.aux
 pdflatex --interaction=nonstopmode paper.tex
 pdflatex --interaction=nonstopmode paper.tex
-#
+
+# diff
+pdflatex --interaction=nonstopmode paper-diff.tex
+bibtex paper-diff.aux
+pdflatex --interaction=nonstopmode paper-diff.tex
+pdflatex --interaction=nonstopmode paper-diff.tex
+
+# SI
 pdflatex --interaction=nonstopmode -shell-escape SI.tex
 bibtex SI.aux
 pdflatex --interaction=nonstopmode -shell-escape SI.tex
